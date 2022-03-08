f4a7b91663b025cffcbe148f67e14028f3b7ad01
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-03-08 10:51:05 -0600

    Add code for getting taxonomy and sequences

--- ICTV_VIRIDIC.R
+++ ICTV_VIRIDIC.R
@@ -1,4 +1,47 @@
+# can install rentrez two ways. 
+# install.packages('rentrez')  OR 
+# library(devtools)
+# install_github("ropensci/rentrez") # this way will give you the latest version
+
+# csv file is from saving the third sheet as a csv file from Microsoft Excel
 ictvxl <- read.csv('~/Documents/ICTV.csv')
 View(ictvxl)
 specieslist <- ictvxl$Species
 length(specieslist)
+
+# Note: these for-loops will take a long time to run if looking at more than 1000 species in specieslist
+# Task: How to make this process efficient?
+
+
+# looking at the first 200 species 
+# when running the following lines of code, R will give error message that the vector ids is empty
+# and at that point accnsp/entrez_search returns 0 hits - stops at Icerudivirus SIRV3
+# after specieslist[19] there are "0" values in tax_lineagsp
+# this for-loop will extract the taxonomic lineage of a given species - necessary for when start working with VIRIDIC
+# if accnsp$count == 0 then that means there were no hits found for that specific species - these also will be in tax_lineagsp
+# and will not start with Viruses as they did not go through the entrez_fetch function
+tax_lineagsp <- rep(0,24)
+for (i in 1:24) {
+  accnsp <- entrez_search(db="taxonomy", term=specieslist[i])
+  if (accnsp$count != 0) {
+    tax_recsp <- entrez_fetch(db="taxonomy", id=accnsp$ids, rettype="xml", parsed=TRUE)
+    #class(tax_recsp)
+    tax_listsp <- XML::xmlToList(tax_recsp)
+    tax_lineagsp[i] <- tax_listsp$Taxon[["Lineage"]]
+  } else {
+    tax_lineagsp[i] <- specieslist[i]
+  }
+}
+
+# writing out the fasta files for specieslist[1] and specieslist[2]
+# if accnsp$count == 0 then that means there were no hits found for that specific species
+for (i in 1:2) {
+  accnsp <- entrez_search(db="nucleotide", term=specieslist[i])
+  if (accnsp$count != 0) {
+    tax_recsp <- entrez_link(dbfrom="nucleotide", id=accnsp$ids, rettype="fasta",db="nuccore")
+    all_recs <- entrez_fetch(db="nuccore", id=tax_recsp$links$nuccore_nuccore_gbrs, rettype="fasta")
+    #cat(strwrap(substr(all_recs,1,2147483647)), sep="\n")
+    #tax_seqssp[i] <- c(strwrap(all_recs))
+    write(all_recs, file="~/Documents/myfile.fasta",append = TRUE)
+  }
+}
