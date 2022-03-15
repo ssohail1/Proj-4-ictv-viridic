48f8bfe73969cbc6ca3dc46823fd5b86c54f55bd
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-03-15 11:48:24 -0500

    Add code for for-loop to count hits vs no hits
    counting how many species have 0 hits (countze) and how many do have hits (count)
    add comments about what to do next

--- ICTV_VIRIDIC.R
+++ ICTV_VIRIDIC.R
@@ -9,6 +9,7 @@ View(ictvxl)
 specieslist <- ictvxl$Species
 length(specieslist)
 
+# Moving Forward: need to address this issue when retrieving accession IDs and fasta sequences
 # Note: these for-loops will take a long time to run if looking at more than 1000 species in specieslist
 # To-do: How to make this process efficient?
 
@@ -17,41 +18,26 @@ library(rentrez)
 # when running the following lines of code, R will give error message that the vector ids is empty
 # and at that point accnsp/entrez_search returns 0 hits - stops at Icerudivirus SIRV3
 # after specieslist[19] there are "0" values in tax_lineagsp
-# if accnsp$count == 0 then that means there were no hits found for that specific species - these also will be in tax_lineagsp
-# and will not start with Viruses as they did not go through the entrez_fetch function
+# if accnsp$count == 0 then that means there were no hits found for that specific species
 # There are species in the ICTV master species list that result in no hits found when running entrez_search()
 
-# writing out the fasta files for specieslist[1] and specieslist[2]
-# if accnsp$count == 0 then that means there were no hits found for that specific species
-
-''' jacob code - not functional
-accnsplist <- rep(0,length(specieslist))
+# this is a loop to see how many of the 9110 species give out 0 hits from NCBI
+# and how many species have hits from NCBI
+count <- 0
+countze <- 0
 for (i in 1:length(specieslist)) {
-  accnsplist[i] <- entrez_search(db="nucleotide", term=specieslist[i], retmax=length(specieslist))}
-
-ictvxl <- read.csv("/Users/jgrandinetti/Downloads/ICTV.csv")
-View(ictvxl)
-specieslist <- ictvxl$Species
-length(specieslist)
-
-accnsplist <- rep(0,length(specieslist))
-count = 0
-
-for (i in 1:length(specieslist)) {
-  accnsplist[i] <- entrez_search(db="nuccore", term=specieslist[i], retmax=length(specieslist))
-     
-    efetch.batch(id, chunk_size = 200, rettype = NULL,
-                   retmode = NULL, retmax = NULL, strand = NULL,
-                   seq_start = NULL, seq_stop = NULL, complexity = NULL)
-
-  if (accnsplist[i]$count != 0) {
-    count + 1
-    tax_recsplist <- rep(0,count)
-    tax_recsp <- entrez_link(dbfrom="nucleotide", id=accnsp$ids, rettype="fasta",db="nuccore")
+  accnsplist <- entrez_search(db="nuccore", term=specieslist[i], retmax=length(specieslist))
+  if (accnsplist$count != 0) {
+    count <- count + 1
+  } else {
+    if (accnsplist$count == 0) {
+      countze <- countze + 1
+    }
   }
 }
-'''
 
+# this is test code to see if able to retrieve fasta sequences from NCBI
+# this looks at the first two species in the specieslist
 for (i in 1:2) {
   accnsp <- entrez_search(db="nucleotide", term=specieslist[i])
   if (accnsp$count != 0) {
@@ -59,6 +45,7 @@ for (i in 1:2) {
     all_recs <- entrez_fetch(db="nuccore", id=tax_recsp$links$nuccore_nuccore_gbrs, rettype="fasta")
     #cat(strwrap(substr(all_recs,1,2147483647)), sep="\n")
     #tax_seqssp[i] <- c(strwrap(all_recs))
+    # writing out the fasta files for specieslist[1] and specieslist[2]
     write(all_recs, file="~/Documents/myfile.fasta",append = TRUE)
   }
 }
