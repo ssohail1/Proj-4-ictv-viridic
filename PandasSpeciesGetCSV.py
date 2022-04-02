2fed34480e36199a7d3cd1be08b62d63dc574912
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-04-02 13:08:44 -0500

    Update PandasSpeciesGetCSV.py

--- PandasSpeciesGetCSV.py
+++ PandasSpeciesGetCSV.py
@@ -1,20 +1,20 @@
+#This code automates getting the 3rd sheet from the ICTV master species excel file 
 
-#to install pandas do this command on your terminal
-#pip install pandas
-
-#this code automates getting the 3rd sheet from the ICTV master species excel file 
+#In order to install pandas on Python3, use this command in terminal -
+#sudo apt install python3-pandas
+#The rest of the documentation is done in Python3.
 
 import pandas as pd
+import csv
 
 #read sheet directly into data frame
 #df = pd.read_excel(fileName, SheetName)
-df= pd.read_excel('ICTV Master Species List 2020.v1', 'ICTV2020 Master Species List#36')
+df= pd.read_excel(r'ICTV Master Species List 2020.v1.xlsx', 'ICTV2020 Master Species List#36')
 
 #make list of species data using tolist (method)
 data = df['Species'].tolist()
 
 #write to Species.csv file to be used in our for loop 
-with open ('Species.csv', 'w') as f:
+with open ('species.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(header)
     writer.writerow(data)
