f6a4f7a31f7f95a434bc30efce9ce5024b6cda79
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-04-02 11:50:36 -0500

    Update ICTV_VIRIDIC.R

--- ICTV_VIRIDIC.R
+++ ICTV_VIRIDIC.R
@@ -35,12 +35,23 @@ library(rentrez)
 # this is a loop to see how many of the 9110 species give out 0 hits from NCBI
 # and how many species have hits from NCBI
 
+
+# Rhea: Attempting to use esearch (from reutils package) for this for-loop. Having issues and consistently getting errors `Warning: HTTP error: Status 500;`
+# accnsplist <- esearch(specieslist, db = "nuccore", rettype = "count", retmax=length(specieslist))
+# esearch does have 'count' funcitonality which would make this process easier (hopefully)
+
+
+# this is writing out the fasta sequences - modify this to write out the tax_recsp IDs to a text file where each species accession IDs is a new line
+#Jacob: added above request, still getting stuck when species has 0 hits from NCBI. If we can get above for loop to run faster, then we can add in an if statement to
+#second for loop to exclude species if it has 0 matches
+
+# Sidra:
 # this stores species from specieslist[i] that have accnsplist$count == 0 - completed
 ictvxl <- read.csv('/home/ssohail/MasterSpeciesList2020.csv') 
 specieslist <- ictvxl$Species
 
 # accnsplist$count is the number of IDs returned from searching a species
-# Sidra: this loops through the specieslist and if the count of ids is 0 then it appends the species to the specieszero12.txt file
+# this loops through the specieslist and if the count of ids is 0 then it appends the species to the specieszero12.txt file
 # The specieszero12.txt file has 318 species and can use this to filter out/remove species from the specieslist so that we don't
 # have to search for the species that yield 0 hits from NCBI
 for (i in 1:length(specieslist)) {
@@ -50,19 +61,7 @@ for (i in 1:length(specieslist)) {
   }
 }
 
-# Rhea: Attempting to use esearch (from reutils package) for this for-loop. Having issues and consistently getting errors `Warning: HTTP error: Status 500;`
-# accnsplist <- esearch(specieslist, db = "nuccore", rettype = "count", retmax=length(specieslist))
-# esearch does have 'count' funcitonality which would make this process easier (hopefully)
-
-# UPDATE:
-# Sidra: this accounts for the name mismatch in NCBI - for-loop seems to get stuck at random points
-# if it gets stuck after [15] (if the last thing printed is 15) then look at specieslist[16] and change for (i in 1:25) to for (i in 16) and see if it works then
-
-# this is writing out the fasta sequences - modify this to write out the tax_recsp IDs to a text file where each species accession IDs is a new line
-#Jacob: added above request, still getting stuck when species has 0 hits from NCBI. If we can get above for loop to run faster, then we can add in an if statement to
-#second for loop to exclude species if it has 0 matches
-
-# Sidra: this for loop will take the filtered out specieslist file that does not have species that yield 0 hits
+# this for loop will take the filtered out specieslist file that does not have species that yield 0 hits
 # as a trial run I have set the range to be the first 4 entries of specieslist
 for (i in 1:4) {
   #  for (j in accnsp[,i]) {
@@ -89,6 +88,7 @@ write.table(mstore,file="~/Downloads/COMP_383-483_compbio/first4species16fasta.f
 # this mstoremodify variable will need to be filtered to remove repeats of sequences and remove sequences that do not have the header ">NC_number "
 mstoremodify <- read.table(file="~/Downloads/COMP_383-483_compbio/first4species16fasta.fasta")
 
+                
 # Next Steps: modify the mstoremodify variable so to remove repeat sequences and remove sequences that do not have header ">NC_number " OR
                 # can create a function similar to fastaseqretrieval that implements entrez_link instead of entrez_fetch to retrieve accession ids 
                 # that will retrieve fasta sequences with ">NC_number" header
