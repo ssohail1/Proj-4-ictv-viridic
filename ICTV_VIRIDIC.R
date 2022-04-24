6fe3b93cf3882728f3c9e6d2f6ba25840cf1aa0c
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-04-23 22:08:53 -0500

    update
    create single list of accession IDs from the VMR ICTV file for 8719 species
    search for accession IDs for 73 species

--- ICTV_VIRIDIC.R
+++ ICTV_VIRIDIC.R
@@ -53,53 +53,6 @@ length(specieslist[[1]])
 # Sidra
 library(rentrez)
 
-# Need to write a python script that will parse through the above excel file (using pandas) and extract the columns with the ICTV names, NCBI names, GenBank and RefSeq accession ids, and EorA labels
-
-speciesexcel <- read.csv(file= "~/Downloads/VMR 18-191021 MSL36.xlsx", sep= '\t') # VMR 18-191021 MSL36.xlsx is the excel file - Virus Metadata Repository from October 19 2021
-speciesinfo <- read.table(file="~/Downloads/COMP_383-483_compbio/ICTVspeciesinfo.txt",header = TRUE,sep = '\n') # ICTVspeciesinfo.txt has the ICTV names from excel file
-speciesinfo <- data.frame(speciesinfo)
-speciesNCBI <- read.csv(file = "~/Downloads/COMP_383-483_compbio/ncbinames.txt",sep = '\n') # ncbinames.txt has ncbi names from excel file
-speciesinfoaccessions <- read.csv(file = "~/Downloads/COMP_383-483_compbio/accessionidslist.txt",sep = '\t') # accessionidslist.txt has GenBank and RefSeq ids from excel file
-
-# additionally need to parse through the RefSeq id accessions to replace the blank column entries with "zero"
-
-speciesexemplaroraddnl <- read.table(file = "~/Downloads/eora.txt",header = FALSE, sep = '\n') # eora.txt has exemplar or additional labels for each species from the excel file
-
-# combine the previous datasets into one dataframe
-speciesinfocomplete <- cbind(speciesinfo,speciesNCBI,speciesinfoaccessions,speciesexemplaroraddnl)
-colnames(speciesinfocomplete) <- c("ICTVnames", "NCBInames", "GenBankIDs", "RefSeqIDs","ExemplarOrAdditional")
-
-zerospecies <- list()
-for (i in 1:length(speciesinfocomplete$RefSeqIDs)) {
-  if (speciesinfocomplete$RefSeqIDs[i] == "zero") {
-    zerospecies <- c(zerospecies, speciesinfocomplete[i,])
-  }
-}
-length(zerospecies) # 18515 total -> 5 columns so 18515/5 = 3703 species do not have the associated RefSeq accession id
-
-speciesinforemovezero <- cbind(speciesinfo,speciesNCBI,speciesinfoaccessions,speciesexemplaroraddnl)
-colnames(speciesinforemovezero) <- c("ICTVnames", "NCBInames", "GenBankIDs", "RefSeqIDs","ExemplarOrAdditional")
-
-# run this until length of speciesinforemovezero$RefSeqIDs is 3703 and the error message stops
-for (i in 1:length(speciesinforemovezero$RefSeqIDs)) {
-  if (speciesinforemovezero$RefSeqIDs[i] != "zero") {
-    speciesinforemovezero <- speciesinforemovezero[-i,]
-  }
-}
-length(speciesinforemovezero$RefSeqIDs)
-
-# Removing "A" or additional species because they are repeats and not present in the ICTV Master Species List excel file
-
-# run this until count1 is 1246 and the error message stops
-count1 <- 0
-for (i in 1:length(speciesinforemovezero$ExemplarOrAdditional)) {
-  if (speciesinforemovezero$ExemplarOrAdditional[i] == "A") {
-    count1 <- count1 + 1
-    speciesinforemovezero <- speciesinforemovezero[-i,]
-  }
-}
-count1 # there are 1246 "A" in virus metadata
-
 # modifying the pandas python species output to exclude species that give zero hits
 species <- read.table(file="~/Downloads/COMP_383-483_compbio/species.csv", header= TRUE, sep = "\n") # from Pandas python script
 
@@ -144,30 +97,105 @@ for (i in 1:length(speciescsv)) {
 length(speciescsv)
 write.table(speciescsv, file = "~/Downloads/COMP_383-483_compbio/speciesmodifnozeros.txt", row.names = FALSE, col.names = FALSE)
 
-species <- read.csv(file = "~/Downloads/COMP_383-483_compbio/speciesmodifnozeros.txt",header = FALSE) # where the species with zero hits are removed
-species <- data.frame(species)
-speciesaccession <- list()
-inds <- list()
-# notmatch <- list()
+species <- read.csv("~/Downloads/COMP_383-483_compbio/speciesmodifnozeros.txt",header = FALSE)
+# vmr.csv is file from parsing through the VMR excel file in python
+speciesinfo <- read.csv("~/Downloads/vmr.csv")
+colnames(speciesinfo) <- c("ICTVnames", "NCBInames", "ExemplarOrAdditional", "GenBankIDs", "RefSeqIDs")
 
-# Run once
-for (i in 1:length(species$V1)) {
-  for (j in 1:length(speciesinforemovezero$ICTVnames)) {
-    if (species$V1[i] == speciesinforemovezero$ICTVnames[j]) {
-      inds <- c(inds,i)
-      speciesaccession <- c(speciesaccession, species$V1[i])
-    } #else {
-      #notmatch <- c(notmatch, speciesinforemovezero$ICTVnames[j])
-    #}
+cou122 <- 0
+for (i in 1:length(speciesinfo$RefSeqIDs)) {
+  # if there are empty/blanks in the RefSeq id then put zero 
+  if (speciesinfo$RefSeqIDs[i] == "") {
+    cou122 <- cou122 + 1 # there are 3704 blanks in the RefSeqIDs column
+    speciesinfo$RefSeqIDs[i] <- "zero"
   }
 }
+count1 <- 0
+for (i in 1:length(speciesinfo$ExemplarOrAdditional)) {
+  # remove the rows where the E or A column is "A"
+  if (speciesinfo$ExemplarOrAdditional[i] == "A") {
+    count1 <- count1 + 1 # there are 1453 rows that have A 
+    speciesinfo<- speciesinfo[-i,]
+  }
+}
+write.csv(speciesinfo,file= "~/Downloads/COMP_383-483_compbio/vmrupd.csv",row.names = FALSE)
 
-length(speciesinforemovezero$ICTVnames) # 2457
-length(inds) # 2216
-length(speciesinforemovezero$ICTVnames) == length(inds) # FALSE
-length(speciesaccession) == length(inds) # TRUE
+speciesinfo <- read.csv("~/Downloads/COMP_383-483_compbio/vmrupd.csv",header = TRUE)
+speciesinfo1 <- data.frame(matrix(ncol = length(colnames(speciesinfo)), nrow = length(rownames(species))))
+count <- 0
+for (i in 1:length(speciesinfo[,2])) {
+  for (j in 1:length(species[,1])) {
+    # if the ICTV name from the vmrupd.csv file (speciesinfo) matches
+    # the ICTV name in speciesmodifnozeros txt file (species) then 
+    # add that row from speciesinfo to speciesinfo1
+    if (speciesinfo$ICTVnames[i] == species$V1[j]) {
+      speciesinfo1[j,] <- speciesinfo[i,]
+    } else {
+      count <- count + 1
+    }
+  }
+}
+co <- 0
+colnames(speciesinfo1) <- colnames(speciesinfo)
+for (i in 1:length(speciesinfo1$GenBankIDs)) {
+  if (speciesinfo1$GenBankIDs[i] == "") {
+    co <- co + 1  # 73
+    # there are 73 ICTV species that do not have GenBank IDs
+    speciesinfo1$GenBankIDs[i] <- "zero"
+  }
+}
+write.csv(speciesinfo1,file = "~/Downloads/COMP_383-483_compbio/speciesupd.csv",row.names = FALSE)
+speciesinfo1 <- read.csv("~/Downloads/COMP_383-483_compbio/speciesupd.csv",header=TRUE)
+species1 <- data.frame(matrix(ncol = length(colnames(speciesinfo1)), nrow = co))
+coun <- 0
+for (i in 1:length(speciesinfo1[,1])) {
+  if (speciesinfo1$GenBankIDs[i] == "zero") {
+    coun <- coun + 1   # 73
+  }
+}
+# for (i in 1:length(speciesinfo1[,1])) {
+#   for (j in 1:length(rownames(species1))) {
+#   if (speciesinfo1$GenBankIDs[i] == "zero") {
+#     species1[j,] <- speciesinfo1[i,]
+#   }
+#   }
+# }
+colnames(species1) <- colnames(speciesinfo1)
+speciesacc <- data.frame(matrix(ncol = 2, nrow = 8792))
 
-write.table(speciesaccession, file = "~/Downloads/COMP_383-483_compbio/speciestolookfor.txt",row.names = FALSE, col.names = FALSE)
+# making a list of available accession IDs for ICTV species
+speciesacc$X1 <- speciesinfo1$ICTVnames
+for (i in 1:length(speciesinfo1[,1])) {
+  # when RefSeqIDs is not equal to zero add that ID to 
+  # speciesacc and when GenBankIDs is not equal to zero than
+  # add that ID to speciesacc otherwise add zero
+  if (speciesinfo1$RefSeqIDs[i] != "zero") {
+    speciesacc$X2[i] <- speciesinfo1$RefSeqIDs[i]
+  } else if (speciesinfo1$GenBankIDs[i] != "zero") {
+    speciesacc$X2[i] <- speciesinfo1$GenBankIDs[i]
+  } else {
+    speciesacc$X2[i] <- "zero"
+  }
+}
+colnames(speciesacc) <- c("ICTVnames","accessionIDs")
+# adding ICTV species that have zero as ID to sp
+sp <- vector()
+for (i in 1:length(speciesacc$ICTVnames)) {
+  if (speciesacc$accessionIDs[i] == "zero") {
+    sp <- c(sp,speciesacc$ICTVnames[i])
+  }
+}
+# removing ICTV species that have zero as ID from speciesacc
+for (i in 1:length(speciesacc$ICTVnames)) {
+  if (speciesacc$accessionIDs[i] == "zero") {
+    speciesacc <- speciesacc[-i,]
+  }
+}
+write.table(sp, file = "speciestosearch.txt",row.names = FALSE)
+write.csv(speciesacc,file = "~/Downloads/COMP_383-483_compbio/speciesaccessions.csv",row.names = FALSE)
+
+species <- read.csv("/home/ssohail/speciesaccessions.csv", header = TRUE)
+IDsaccess <- species$accessionIDs
 
 ################################
 
