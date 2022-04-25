26a0c36f2ce1f328c5c6f0de89330b2be12dab1d
jgrandi7 <83033313+jgrandi7@users.noreply.github.com>
2022-04-25 15:40:30 -0500

    Update removequotes.py

--- removequotes.py
+++ removequotes.py
@@ -1,7 +1,11 @@
+#because prior step output includes unnecessary quotations that cannot be read in a fasta file, we need to remove them for blast to run
+
 print('Enter filename:')
-filename = input()
+filename = input() #use full path name here!
+
 print('Enter outputname:')
 outputname = input()
+
 with open(filename, 'r') as f, open(outputname, 'w') as fo:
     for line in f:
         fo.write(line.replace('"', '').replace("'", ""))
