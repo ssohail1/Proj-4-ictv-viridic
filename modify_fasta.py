9fbcaef64d9a871253c02f170d4dccf958843d62
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-04-28 22:31:04 -0500

    update
    modify the multifasta file to include ICTV species names and replace spaces with underscores

--- modify_fasta.py
+++ modify_fasta.py
@@ -19,3 +19,58 @@ with open("/home/rprag/finfasta3.fasta", 'r') as inputs, open("/home/rprag/finfa
     k.description = k.id
   # Write result to file  
   SeqIO.write(my_dict.values(), outputs, 'fasta')
+
+
+##################################
+
+
+# Sidra:
+
+import os
+import csv
+
+  # copy fasta file to a text file
+def filetotxt(file):
+    totxtcmmd = 'cp '+ file + '.fasta ' + file + '.txt'
+    os.system(totxtcmmd)
+filetotxt('finfasta3')
+
+# split at ">" so that each fasta sequence is separated
+multifasta = open('finfasta3.txt','r').read().split('">')
+
+# file with the modified headers will be the ictvheadermultifasta.txt file
+outfile = open('ictvheadermultifasta.txt','w')
+
+# looping through the multifasta file
+for i in range(0,len(multifasta)):
+	# parsing through the speciesaccessions csv file
+    with open('speciesaccessions.csv', 'r') as file:
+    	# reader is the row in the speciesaccessions csv file
+        reader = csv.reader(file)
+        # row contains the ICTV species names (row[0]) and the accession ID (row[1])
+        for row in reader:
+            # if a sequence in the multifasta starts with a row[1] accession ID
+            if multifasta[i].startswith(str(row[1])):
+                rolen = len(row[1]) # taking the length of the accession ID in row[1]
+                ind = multifasta[i].index(row[1]) # getting the index of where row[1] is in 
+                # the multifasta sequence - this index does not include the '>' so add it here
+                # row[0] is the ICTV species name
+                multifasta[i] = '>' + multifasta[i][ind:rolen+3] + row[0] + '_' + multifasta[i][rolen+3:]
+                # write the multifasta[i] to outfile
+                outfile.write(multifasta[i])
+# close outfile
+outfile.close()
+
+multifasta = open('ictvheadermultifasta.txt','r').read().split('">')
+
+outfile = = open('ictvheadermultifasta_fin.txt','w')
+
+for i in range(0,len(multifasta)):
+  multifasta[i] = multifasta[i].replace(' ','_')
+  outfile.write(multifasta[i])
+oufile.close()
+
+def filetofasta(file):
+    totxtcmmd = 'cp '+ file + '.txt ' + file + '.fasta'
+    os.system(totxtcmmd)
+filetofasta('ictvheadermultifasta_fin')
