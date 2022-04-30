7dcc853cdc5b58258700102ea4566bcdc6aaeef4
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-04-30 16:32:57 -0500

    add run1.py
    this file will be building the database by downloading the fasta files and then will modify headers to include ICTV species names and change spaces to underscores

---
+++ run/run1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 30 16:04:51 2022
+
+@author: sidra
+"""
+import os
+
+
+runcommand = 'nohup Rscript fullfastadownload.R &'
+os.system(runcommand)
+
+
+runcommand = 'nohup python modify_fasta.py &'
+os.system(runcommand)
