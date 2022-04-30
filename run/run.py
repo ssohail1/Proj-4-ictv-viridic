7ff10ad5c4fa0829e20c3428389b6d8ba4e7eb3a
jgrandi7 <83033313+jgrandi7@users.noreply.github.com>
2022-04-29 22:26:53 -0500

    Update run.py

--- run/run.py
+++ run/run.py
@@ -1,6 +1,32 @@
-#outline
-#need: user input and database
-#1) python removequotes
-#2) commandline blast
-#3) python blastsort
-#4) python distance calculations
+"""
+Created on Fri Apr 29 21:51:48 2022
+
+@author: jgrandinetti
+"""
+
+your_path = '/home/jgrandinetti/Proj-4-ictv-viridic/run' # change this to whatever folder you have your files downloaded to
+your_fasta = 'SIRV3.fasta' #change the name of this to the fasta you are using as input
+
+import os
+
+os.system('cd ' + your_path)
+
+
+#### BLAST CODE #### MAKE SURE THAT YOU HAVE BLAST INSTALLED ON YOUR SYSTEM OR THIS WILL NOT WORK
+
+os.sytem('makeblastdb -in finfasta3r.fasta -dbtype nucl -out results/customdb')
+
+os.system('makeblastdb -in ' + your_fasta + ' -dbtype nucl -out results/inputdb')
+
+os.system('blastn -db results/customdb -query ' + your_fasta + ' -out results/blastoutA.csv -evalue 1 -num_threads 12 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen"')
+
+os.system('blastn -db results/inputdb -query finfasta3r.fasta -out results/blastoutB.csv -evalue 1 -num_threads 12 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen" ')
+
+#### ##### #####
+
+os.system('python3 blastsort.py')
+
+os.system('python3 distancecalc.py')
+
+
+os.system()
