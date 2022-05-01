f895c6c0e9b3fc979b0950746e3d260729e602a6
jgrandi7 <83033313+jgrandi7@users.noreply.github.com>
2022-04-30 21:48:08 -0500

    Update run2.py

--- run/run2.py
+++ run/run2.py
@@ -4,7 +4,6 @@ Created on Fri Apr 29 21:51:48 2022
 @author: jgrandinetti
 """
 
-your_path = '/home/jgrandinetti/run' # change this to whatever folder you have your files downloaded to
 your_fasta = 'SIRV3.fasta' #change the name of this to the fasta you are using as input
 
 import os
@@ -14,13 +13,13 @@ os.system('cd ' + your_path)
 
 #### BLAST CODE #### MAKE SURE THAT YOU HAVE BLAST INSTALLED ON YOUR SYSTEM OR THIS WILL NOT WORK
 
-os.sytem('makeblastdb -in finfasta3r.fasta -dbtype nucl -out results/customdb')
+os.sytem('makeblastdb -in sequencesr.fasta -dbtype nucl -out results/customdb')
 
 os.system('makeblastdb -in ' + your_fasta + ' -dbtype nucl -out results/inputdb')
 
 os.system('blastn -db results/customdb -query ' + your_fasta + ' -out results/blastoutA.csv -evalue 1 -num_threads 12 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen"')
 
-os.system('blastn -db results/inputdb -query finfasta3r.fasta -out results/blastoutB.csv -evalue 1 -num_threads 12 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen" ')
+os.system('blastn -db results/inputdb -query sequencesr.fasta -out results/blastoutB.csv -evalue 1 -num_threads 12 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen" ')
 
 #### ##### #####
 
