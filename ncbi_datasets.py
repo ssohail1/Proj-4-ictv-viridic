5e64ee0783833a672e01fff1ea7753496a4e517a
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-05-01 19:15:15 -0500

    Update ncbi_datasets.py

--- ncbi_datasets.py
+++ ncbi_datasets.py
@@ -1,3 +1,5 @@
+# To find more information regarding NCBI Datasets: https://github.com/ncbi/datasets/tree/master/client_docs/python
+
 # To install NCBI Datasets, use the following command in terminal:
     #curl -o datasets 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets'
     #curl -o dataformat 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/dataformat'
@@ -6,15 +8,20 @@
 import os
 
 def datasets():
+    # Command to run NCBI datasets
     command = 'datasets download genome accession --inputfile accessions.txt --filename datasets --exclude-genomic-cds --exclude-gff3 --exclude-protein --exclude-rna'
     os.system(command)
+    # Create output file in fasta format
     os.system('cat *.fasta > sequences.fasta')
     
-def input_commd():
+def input_cmmd():
+    # Read input file and strip lines
+        # 'accessions.txt' is a list of all the accession ids
     with open("accessions.txt", 'r') as f:
         sequence = f.read().strip()
+    # Use the accession ids and run them through NCBI datasets
     if sequence:
         datasets()
-       
+
+# Execute command
 input_cmmd()
-#sometimes works?
