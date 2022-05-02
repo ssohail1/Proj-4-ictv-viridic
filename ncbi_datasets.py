a0f89a455add9a1e60127ea4d776757819c084b9
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-05-01 19:17:01 -0500

    Update ncbi_datasets.py

--- ncbi_datasets.py
+++ ncbi_datasets.py
@@ -9,6 +9,7 @@ import os
 
 def datasets():
     # Command to run NCBI datasets
+        # Only need RefSeqGenBankNumber_Species_PartialOrCompleteGenome
     command = 'datasets download genome accession --inputfile accessions.txt --filename datasets --exclude-genomic-cds --exclude-gff3 --exclude-protein --exclude-rna'
     os.system(command)
     # Create output file in fasta format
