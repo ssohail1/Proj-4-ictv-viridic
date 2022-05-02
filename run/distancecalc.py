233a5045dcb42eacbef84cedda91befd39b5898d
jgrandi7 <83033313+jgrandi7@users.noreply.github.com>
2022-05-02 00:54:29 -0500

    Update distancecalc.py

--- run/distancecalc.py
+++ run/distancecalc.py
@@ -12,8 +12,8 @@ import os
 
 current = os.getcwd()
 
-fileA = pd.read_csv(current + '/totidentA.csv', sep = ',')
-fileB = pd.read_csv(current + '/totidentB.csv', sep = ',')
+fileA = pd.read_csv(current + '/results/totidentA.csv', sep = ',')
+fileB = pd.read_csv(current + '/results/totidentB.csv', sep = ',')
 
 fileA.columns = ['index','input_accessionID', 'database_accessionID', 'qstart', 'qend', 'nident','lenA','totidentAB']
 fileB.columns = ['index','database_accessionID', 'input_accessionID', 'qstart', 'qend', 'nident','lenB','totidentBA']
@@ -29,17 +29,19 @@ def distance_calc(A, B, AB, BA):
     return distAB
 
 fileAB['distanceAB'] = fileAB.apply(lambda x: distance_calc(x['lenA'], x['lenB'], x['totidentAB'], x['totidentBA']), axis=1)
+fileAB = fileAB.sort_values(by = 'distanceAB', ascending = True)
 
-fileABshort = fileAB[['input_accessionID','database_accessionID','distanceAB']].drop_duplicates(subset=None, keep='first', inplace=False).head(100)
+fileABshort = fileAB[['input_accessionID','database_accessionID','distanceAB']].drop_duplicates(subset=None, keep='first', inplace=False)
 
 with open(current + '/results/results.csv','w') as f: #open csv file to write
     fileABshort.to_csv(f) #write dataframe from list to csv
 
-result = fileABshort.pivot_table(index='input_accessionID', columns='database_accessionID', values='distanceAB')
+fileABshort = fileABshort.head(100)
+fileABshort = fileABshort.pivot_table(index='input_accessionID', columns='database_accessionID', values='distanceAB')
 
-fig, ax = plt.subplots(figsize=(len(result.columns),len(result)))
+fig, ax = plt.subplots(figsize=(len(fileABshort.columns),len(fileABshort)))
 
-heatmap = sns.heatmap(result, annot=True, fmt="g", cmap='viridis', linewidths=5)
+heatmap = sns.heatmap(fileABshort, annot=True, fmt="g", cmap='viridis', linewidths=5)
 
 figure = heatmap.get_figure()
-figure.savefig(current + '/results/heatmap.png', dpi = 300, bbox_inches='tight')
+figure.savefig(current +'/results/heatmap.png', dpi = 300, bbox_inches='tight')
