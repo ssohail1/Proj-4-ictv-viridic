1f5c066c30ca85efba1d54fa4e91b2a0c2724927
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-05-06 15:15:10 -0500

    Update fullfastadownload.R

--- sample_data/fullfastadownload.R
+++ sample_data/fullfastadownload.R
@@ -1,11 +1,15 @@
 library(rentrez)
 
+# length of speciesaccessions.csv is 127 species
+
 specacc <- read.csv("speciesaccessions.csv",header=TRUE)
 
+# entrez_fetch function
 fastaseqretrieval <- function(search_term) {
   return(sapply(search_term, function(s) entrez_fetch(db="nucleotide",id=s,rettype="fasta")))
 }
 
+# the range will need to be changed if not using the speciesaccessions.csv file with length of 127 species
 i1 <- 1
 ids <- specacc[i1:(i1+99),2] # accession numbers for the first 100 species
 mstore1 <- fastaseqretrieval(ids)
