ea24f044121f56fcc7ecba7c82e7b952655ac9a1
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-05-06 14:33:28 -0500

    Update accessions.R

--- run/accessions.R
+++ run/accessions.R
@@ -1,25 +1,33 @@
 library(rentrez)
+
+# 73 species in look
 look <- read.table('/home/ssohail/speciestolookfor.txt',header=FALSE)
+
+# entrez_search function
 accessids <- function(search_term){
-  return(sapply(search_term, function(s) entrez_search(db="nucleotide", term=s, retmax=length(look$V1))$ids))
+  return(sapply(search_term, function(s) entrez_search(db="nucleotide", term=s, retmax=length(look$V1))$ids)) # the retmax limits the output to 73 accession IDs
 }
-# accsns <- accessids(look$V1)
+
+# entrez_link function
 entrlinkfasta <- function(search_term){
   return(sapply(search_term, function(s) entrez_link(dbfrom="nucleotide", id=s, rettype="fasta",db="nuccore")$links$nuccore_nuccore_gbrs))
 }
+
+# entrez_fetch function
 fastaseqretrieval <- function(search_term){
   return(sapply(search_term, function(s) entrez_fetch(db="nucleotide",id=s,rettype="fasta")))
 }
 
-# accsns_save <- accsns
-
+# Retrieving accession IDs for the 73 species
 accsns <- accessids(look$V1)
-print(length(accsns[[1]]))
-count <- 0
-# entliids <- vector()
-sa_ve <- vector()
-# run this until length changes from 73 to 50 - run 3 times
 
+# this block of code should be run if the number of accession IDs for a species are more than 80
+# if have less than 80 accession IDs then skip to the bottom where you would need to change acc50 to accsns because this block of code was skipped
+########################################################################
+# print(length(accsns[[1]]))
+count <- 0
+sa_ve <- vector()
+# function to remove species with more than 80 accession IDs as rentrez cannot run when more than 80 accession IDs are input     
 idsacc <- function(accsns, sa_ve, count) {
   for (i in 1:length(accsns)){
     if (i <= length(accsns)) {
@@ -34,7 +42,7 @@ idsacc <- function(accsns, sa_ve, count) {
   }
   return(accsns)
 }
-#accsns <- list(accsns)
+# Running this to remove species with more than 80 accession IDs
 for (i in 1:3) {
   if (i <= 3) {
     acc23 <- idsacc(accsns,sa_ve,count)
@@ -54,23 +62,26 @@ idsacc <- function(accsns, sa_ve, count) {
   }
   return(accsns)
 }
-print(length(acc23))
+# print(length(acc23))
 for (i in 1:3) {
   if (i <= 3) {
     acc50 <- idsacc(acc23,sa_ve,count)
   }
 }
-print(length(acc50))
+########################################################################
+
+# looping through acc50 where each species has a varied number of accession IDs
+# print(length(acc50))
 for (i in 1:length(acc50)) {
   idlinken <- entrlinkfasta(acc50[[i]])
   for (j in 1:length(idlinken)) {
-    nullvals <- is.null(idlinken[[j]])
+    nullvals <- is.null(idlinken[[j]]) # the entrez_link function can yield NULL values so those are filtered out
     if (nullvals == FALSE) {
-      seqfas <- fastaseqretrieval(idlinken[[j]])
-      sa <- c(names(acc50[i]))
-      print(length(idlinken))
+      seqfas <- fastaseqretrieval(idlinken[[j]]) # retrieving fasta sequences if null values are not present
+      sa <- c(names(acc50[i])) # keeping track of which ICTV species fasta sequence is being retrieved
+      print(length(idlinken)) # check whether code is running
       write.table(seqfas, file="/home/ssohail/fasta50_finalfin.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
-      write.table(sa, file="/home/ssohail/accessandname_finalfin.txt",append=TRUE, row.names = FALSE,col.names=FALSE)
+      write.table(sa, file="/home/ssohail/accessandname_finalfin.txt",append=TRUE, row.names = FALSE,col.names=FALSE) # will be used to modify headers of fasta50_finalfin.fasta in the modify_fasta.py script
     }
   }
 }
