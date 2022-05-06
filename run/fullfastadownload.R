1a1b0a782db82d6752c831647cf90bd6d01ff862
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-05-06 14:38:31 -0500

    Update fullfastadownload.R

--- run/fullfastadownload.R
+++ run/fullfastadownload.R
@@ -1,25 +1,27 @@
 library(rentrez)
 
+# to avoid errors from NCBI and rentrez the specacc is run in multiple parts
+# length of speciesaccessions.csv file is 8717 species
+
 specacc <- read.csv("/home/ssohail/speciesaccessions.csv",header=TRUE)
 
-#start1 <- Sys.time()
+# entrez_fetch function
 fastaseqretrieval <- function(search_term){
   return(sapply(search_term, function(s) entrez_fetch(db="nucleotide",id=s,rettype="fasta")))
 }
-# seqs <- seq(1,19,1)
-# sp <- vector()
+
 i1 <- 1
 ids <- specacc[i1:(i1+17),2]
 mstore1 <- fastaseqretrieval(ids)
-# sp <- c(sp, specacc[i1:(i1+17),1])
 write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
  
+# the number ranges in seq() is relevant for ICTV MSL version 37 and VMR version 36
+# will need to be modified if using an older MSL version
+
 seqs <- seq(19,6319,100)
-# test: seqs <- seq(119,219,100)
 for (i in seqs) {
   ids <- specacc[i:(i+99),2]
   mstore1 <- fastaseqretrieval(ids)
-  # sp <- c(sp,specacc[i:(i+99),1])
   write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
 }
 
@@ -31,19 +33,14 @@ ids <- specacc[6490:(6490+28),2]
 mstore1 <- fastaseqretrieval(ids)
 write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
 
-# seqs <- seq(6719,8519,100)
 seqs <- seq(6519,8519,100)
 for (i in seqs) {
   ids <- specacc[i:(i+99),2]
   mstore1 <- fastaseqretrieval(ids)
-  # sp <- c(sp,specacc[i:(i+99),1])
   write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
 }
 
 ids <- specacc[8619:(8619+98),2]
 mstore1 <- fastaseqretrieval(ids)
-#sp <- c(sp, specacc[i1,1])
 write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
-#write.table(sp,file="/home/ssohail/listofspeciesfinfasta.txt",row.names=FALSE,col.names=FALSE)
-#end1 <- Sys.time
-#end-start
+
