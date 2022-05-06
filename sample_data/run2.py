5f76e3f890224c5e7cae093ab464a88830796cdc
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-05-05 21:11:08 -0500

    Create run2.py

---
+++ sample_data/run2.py
@@ -0,0 +1,26 @@
+"""
+Created on Fri Apr 29 21:51:48 2022
+
+@author: jgrandinetti
+"""
+
+your_fasta = 'SIRV3.fasta' #change the name of this to the fasta you are using as input
+
+import os
+
+
+#### BLAST CODE #### MAKE SURE THAT YOU HAVE BLAST INSTALLED ON YOUR SYSTEM OR THIS WILL NOT WORK
+
+os.system('makeblastdb -in sequencesr.fasta -dbtype nucl -out results/customdb')
+
+os.system('makeblastdb -in ' + your_fasta + ' -dbtype nucl -out results/inputdb')
+
+os.system('blastn -db results/customdb -query ' + your_fasta + ' -out results/blastoutA.csv -evalue 1 -num_threads 24 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen"')
+
+os.system('blastn -db results/inputdb -query sequencesr.fasta -out results/blastoutB.csv -evalue 1 -num_threads 24 -word_size 7 -reward 2 -penalty -3 -gapopen 5 -gapextend 2 -outfmt "6 qseqid sseqid qstart qend nident qlen" ')
+
+#### ##### #####
+
+os.system('python3 blastsort.py')
+
+os.system('python3 distancecalc.py')
