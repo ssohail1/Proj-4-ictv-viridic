220f2fb76c5e8161a0ce3ed09a34471309f0c4f7
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-05-06 16:10:07 -0500

    Update generating_species_list.R

--- generating_species_list.R
+++ generating_species_list.R
@@ -1,9 +1,10 @@
 '''
-This code assembles the speciesaccessions.csv file. The speciesaccessions.csv file will be input to fullfastadownload.R. 
-The paths in this code need to be changed to your local paths.
+This code assembles the speciesaccessions.csv file and speciestolookfor.txt file. The speciesaccessions.csv file will be input to fullfastadownload.R.
+The speciestolookfor.txt file will be input to accessions.R.
+The input files and generating_species_list.R file need to be in same folder.
 '''
 
-
+# Installation
 # There are several R libraries that need to be installed for this documentation.
 # In order to install `rentrez`, there are two ways:
   # install.packages('rentrez') OR 
@@ -18,50 +19,25 @@ library(rentrez)
   #sudo apt install r-cran-devtools
 library(devtools)
 
-# In order to install reutils, in R - install.packages("reutils")
-library(reutils)
-
-
-# csv file is from saving the third sheet as a csv file from Microsoft Excel
-ictvxl <- read.csv('~/Documents/ICTV.csv')
-View(ictvxl)
-specieslist <- ictvxl$Species
-length(specieslist)
 
 # Note: entrez prints error message when loop through many species
-# To-do: How to have entrez retrieve sequences for all species
-
-# when running the following lines of code, R will give error message that the vector ids is empty
-# and at that point accnsp/entrez_search returns 0 hits - stops at Icerudivirus SIRV3
-# after specieslist[19] there are "0" values in tax_lineagsp
-# if accnsp$count == 0 then that means there were no hits found for that specific species
 # There are species in the ICTV master species list that result in no hits found when running entrez_search()
 
-# this is the count function
-# this is a loop to see how many of the 9110 species give out 0 hits from NCBI
-# and how many species have hits from NCBI
+
 
 # Taking the csv file created using `PandasSpeciesGetCSV.py`,
-  # This file has the list of species names from the ICTV Master Species List
-specieslist <- read.csv("/home/rprag/species.csv")
+# This file has the list of species names from the ICTV Master Species List
+specieslist <- read.csv("species.csv")
 length(specieslist[[1]])
 # This should return as 9110
-# Rhea is working on using this for the analysis
 
-# this is writing out the fasta sequences - modify this to write out the tax_recsp IDs to a text file where each species accession IDs is a new line
-#Jacob: added above request, still getting stuck when species has 0 hits from NCBI. If we can get above for loop to run faster, then we can add in an if statement to
-#second for loop to exclude species if it has 0 matches
-
-
-# Test this chunk of code - change the directories to your local directory
-################################
-# Sidra
 library(rentrez)
 
 # modifying the pandas python species output to exclude species that give zero hits
 species <- read.table(file="~/Downloads/COMP_383-483_compbio/species.csv", header= TRUE, sep = "\n") # from Pandas python script
 
-# run once
+
+# run once -  will take about two days to run ; do not run if have specieszero.txt file
 for (i in 1:length(species[,1])) {
   accnsplist <- entrez_search(db="nuccore", term=species[i,], retmax=length(species[,1]))
   if (accnsplist$count == 0) {
@@ -107,6 +83,7 @@ species <- read.csv("~/Downloads/COMP_383-483_compbio/speciesmodifnozeros.txt",h
 speciesinfo <- read.csv("~/Downloads/vmr.csv")
 colnames(speciesinfo) <- c("ICTVnames", "NCBInames", "ExemplarOrAdditional", "GenBankIDs", "RefSeqIDs")
 
+# do not run if already have vmrupd.csv
 cou122 <- 0
 for (i in 1:length(speciesinfo$RefSeqIDs)) {
   # if there are empty/blanks in the RefSeq id then put zero 
@@ -124,6 +101,8 @@ for (i in 1:length(speciesinfo$ExemplarOrAdditional)) {
   }
 }
 write.csv(speciesinfo,file= "~/Downloads/COMP_383-483_compbio/vmrupd.csv",row.names = FALSE)
+
+
 
 speciesinfo <- read.csv("~/Downloads/COMP_383-483_compbio/vmrupd.csv",header = TRUE)
 speciesinfo1 <- data.frame(matrix(ncol = length(colnames(speciesinfo)), nrow = length(rownames(species))))
@@ -184,7 +163,7 @@ for (i in 1:length(speciesacc$ICTVnames)) {
     sp <- c(sp,speciesacc$ICTVnames[i])
   }
 }
-# removing ICTV species that have zero as ID from speciesacc
+# removing ICTV species that have zero as accession ID from speciesacc
 for (i in 1:length(speciesacc$ICTVnames)) {
   if (speciesacc$accessionIDs[i] == "zero") {
     speciesacc <- speciesacc[-i,]
@@ -195,19 +174,6 @@ write.csv(speciesacc,file = "~/Downloads/COMP_383-483_compbio/speciesaccessions.
 
 species <- read.csv("/home/ssohail/speciesaccessions.csv", header = TRUE)
 IDsaccess <- species$accessionIDs
-
-################################
-
-
-
-# Next Steps: modify the mstoremodify variable so to remove repeat sequences and remove sequences that do not have header ">NC_number " OR
-                # can create a function similar to fastaseqretrieval that implements entrez_link instead of entrez_fetch to retrieve accession ids 
-                # that will retrieve fasta sequences with ">NC_number" header
-# Next Steps: use NCBI created accession number list from ICTV database as batch search NCBI to get equivalent fasta and add to database
-# Next Next Steps: VIRIDIC with in-house database to run user input fasta sequence and determine similarities (Jacob working on figuring out VIRIDIC requirements)
-
-            
-#next steps as of 4/11/2022: we are no longer using VIRIDIC. We will utilize the VICTOR web service from https://ggdc.dsmz.de/victor.php# 
-                
+# speciesaccessions.csv is input to the fullfastadownload.R file
                 
             
