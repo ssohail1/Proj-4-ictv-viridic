699b692fd791d68c1948cd3c0d1180c1432208e5
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2022-05-05 20:18:09 -0500

    Update fullfastadownload.R

--- sample_data/fullfastadownload.R
+++ sample_data/fullfastadownload.R
@@ -1,24 +1,17 @@
 library(rentrez)
 
-specacc <- read.csv("/home/ssohail/speciesaccessions.csv",header=TRUE)
+specacc <- read.csv("speciesaccessions.csv",header=TRUE)
 
-#start1 <- Sys.time()
-fastaseqretrieval <- function(search_term){
+fastaseqretrieval <- function(search_term) {
   return(sapply(search_term, function(s) entrez_fetch(db="nucleotide",id=s,rettype="fasta")))
 }
-# seqs <- seq(1,19,1)
-# sp <- vector()
+
 i1 <- 1
-ids <- specacc[i1:(i1+17),2]
+ids <- specacc[i1:(i1+99),2] # accession numbers for the first 100 species
 mstore1 <- fastaseqretrieval(ids)
-# sp <- c(sp, specacc[i1:(i1+17),1])
-write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
- 
-seqs <- seq(19,6319,100)
-# test: seqs <- seq(119,219,100)
-for (i in seqs) {
-  ids <- specacc[i:(i+99),2]
-  mstore1 <- fastaseqretrieval(ids)
-  # sp <- c(sp,specacc[i:(i+99),1])
-  write.table(mstore1, file="/home/ssohail/finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
-}
+write.table(mstore1, file="finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
+
+i <- 101
+ids <- specacc[i:(i+26),2] # accession numbers for the last 27 species
+mstore1 <- fastaseqretrieval(ids)
+write.table(mstore1, file="finfasta3.fasta",append=TRUE, row.names = FALSE,col.names=FALSE)
